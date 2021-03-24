e606e912d93481b8d0b00766c25cf4c0b45f9904
Zita <Takuji.Takahashi@jp.kpmg.com>
2021-03-24 13:09:49 +0900

    Temporary Commit

---
+++ build/build.ps1
@@ -0,0 +1,46 @@
+function main(){
+<#
+           .SYNOPSIS 
+           
+
+           .DESCRIPTION
+           
+    
+           .PARAMETER InputPath
+           パラメータ
+#>
+    
+}
+
+function aaa
+{
+    Write-Host "hoge"
+
+<#
+.SYNOPSIS
+Hoge.
+
+.DESCRIPTION
+Get-Function displays the name and syntax of all functions in the session.
+
+#>
+
+}
+
+
+function bbb
+{
+    Write-Host "hoge"
+
+<#
+.SYNOPSIS
+HIGE.
+
+.DESCRIPTION
+日本語
+
+#>
+
+}
+
+main();
