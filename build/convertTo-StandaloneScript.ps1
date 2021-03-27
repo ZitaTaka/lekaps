f4fabeb25389a6d2aa0de58217f1f7b045368c0c
Zitataka <ttakahashi34@gmail.com>
2021-03-27 19:19:25 +0900

    Make convertTo-StandaloneScript

--- build/convertTo-StandaloneScript.ps1
+++ build/convertTo-StandaloneScript.ps1
@@ -1,65 +1,132 @@
-function convertTo-StandaloneScript(){
+<#
+--------- --------- --------- --------- --------- --------- --------- --------- --------- --------- --------- --------- 
+.SYNOPSIS 
+    Merge source files to single ps1.file.
+    This is the entry point of mergeToSinglePS.ps1
+
+.DESCRIPTION
+    Replace Import-Module to content of the module.
+    "Import-Module specific_module.ps1" in the inFile will replace the contents of specific_module.ps1.
+    path to specific_module.ps1 should be absolute path or relateve path from inFile.
+
+
+.INPUTS
+    None. You cannot pipe objects.
+
+.OUTPUTS
+    None. Return nothing.
+
+.EXAMPLE
+    convertTo-StandaloneScript.ps1 -inFile "/path/to/input.ps1" -outFile "/path/to/input.ps1"
+
+--------- --------- --------- --------- --------- --------- --------- --------- --------- --------- --------- --------- 
+#>
+
+param(
+    [parameter(mandatory=$true)][String]$inFile,
+        # Specify input .ps1 script file.
+    [parameter(mandatory=$true)][String]$outFile
+        # Specify output .ps1 script file.
+)
+
+<#
+--------- --------- --------- --------- --------- --------- --------- --------- --------- --------- --------- --------- 
+    .SYNOPSIS 
+        Merge source files to single ps1.file.
+        This is the entry point of mergeToSinglePS.ps1
+
+    .DESCRIPTION
+        Replace Import-Module to content of the module.
+        "Import-Module specific_module.ps1" in the inFile will replace the contents of specific_module.ps1.
+        path to specific_module.ps1 should be absolute path or relateve path from inFile.
+
+    .PARAMETER  InFile
+        Specify input .ps1 script file.
+
+    .PARAMETER  OutFile
+        Specify output .ps1 script file.
+
+    .INPUTS
+        None. You cannot pipe objects.
+
+    .OUTPUTS
+        None. Return nothing.
+
+    .EXAMPLE
+        convertTo-StandaloneScript -inFile "/path/to/input.ps1" -outFile "/path/to/input.ps1"
+#>
+function ConvertTo-StandaloneScript(){
     param(
             [parameter(mandatory=$true)][String]$inFile,
+                # Specify input .ps1 script file.
             [parameter(mandatory=$true)][String]$outFile
+                # Specify output .ps1 script file.
     )
-<#
-           .SYNOPSIS 
-           Merge source files to single ps1.file.
-           This is the entry point of mergeToSinglePS.ps1
-
-
-           .DESCRIPTION
-           Replace Import-Module to content of the module.
-           "Import-Module specific_module.ps1" in the inFile will replace the contents of specific_module.ps1.
-           path to specific_module.ps1 should be absolute path or relateve path from inFile.
-    
-           .PARAMETER  InFile
-           Specify input .ps1 script file.
-
-           .PARAMETER  OutFile
-           Specify output .ps1 script file.
-
-           .INPUTS
-           None. You cannot pipe objects.
-
-           .OUTPUTS
-           None. Return nothing.
-
-           .EXAMPLE
-           convertTo-StandaloneScript.ps1 -inFile "/path/to/input.ps1" -outFile "/path/to/input.ps1"
-#>
-    ## Show progress
+    # show message
     Write-Host("Start to convert");
     Write-Host("inFile  "+$inFile);
     Write-Host("outFile "+$outFile);
 
-
-    ## Work on directory inFile placed.
+    # set directory from inFile.
     Set-Location(Split-Path($inFile));
     Write-Host("Current Directiory "+(Get-Location));
 
-    $TmpInc=New-TemporaryFile
-    $TmpBody=New-TemporaryFile
-
+    # read line from inFile
+    # replace "Import-Module" with target module content
     foreach($line in Get-Content($inFile)){
+        # "Import-Module" line
         if($line.trimU() -match "^Import-Module"){
             $modName=($line.replace("Import-Module","")).trim(); 
-            readAll -inFile $modName -outFile $outFile
+            Append-AllScriptLine -inFile $modName -outFile $outFile;
+            Write-Host("replaced `"$line`"");
+        # normal line
         }else{
-            Add-Content -Path $outFile -Value $line
+            Add-Content -Path $outFile -Value $line;
         }        
     }
 
+    # show message
+    Write-Host("Finished");
 }
-function readAll(){
+
+
+<#
+--------- --------- --------- --------- --------- --------- --------- --------- --------- --------- --------- --------- 
+    .SYNOPSIS 
+        Append all lines in the file to the specified outfile
+
+    .DESCRIPTION
+        Read all lines from inFile
+        Append all lines to outFile 
+
+    .PARAMETER  InFile
+        Specify input .ps1 script file.
+
+    .PARAMETER  OutFile
+        Specify output .ps1 script file.
+
+    .INPUTS
+        None. You cannot pipe objects.
+
+    .OUTPUTS
+        None. Return nothing.
+
+    .EXAMPLE
+        Append-AllScriptLine -inFile "/path/to/input.ps1" -outFile "/path/to/input.ps1"
+#>
+function Append-AllScriptLine(){
     param(
             [parameter(mandatory=$true)][String]$inFile,
+                # Specify input .ps1 script file.
             [parameter(mandatory=$true)][String]$outFile
+                # Specify output .ps1 script file.
     )
     foreach($line in Get-Content($inFile)){
         Add-Content -Path $outFile -Value $line
     }
 }
 
-convertTo-StandaloneScript -inFile "C:\Users\TEMP.JP.000\Documents\mana-leka\build\margeToSinglePS.ps1" -outFile "margeToSinglePS.txt";
+# default action
+if ($MyInvocation.InvocationName -ne ".") {
+    convertTo-StandaloneScript -inFile "$inFile" -outFile "$outFile";
+}
